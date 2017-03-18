4221d2cf2007a094aac304ab617f9953e25a6dd5
nikhil <ntnikhilesh@gmail.com>
2017-03-18 18:32:25 +0530

    sending UID as bearer token with Authorization header done

--- functions/index.js
+++ functions/index.js
@@ -1,71 +1,73 @@
-var functions = require('firebase-functions');
+'use strict';
 
-// // Start writing Firebase Functions
-// // https://firebase.google.com/functions/write-firebase-functions
-//
-exports.helloWorld = functions.https.onRequest((request, response) => {
- response.send("Hello from Firebase!");
-})
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/*'use strict';
-
-const functions = require('firebase-functions');
+let functions = require('firebase-functions');
 const admin = require('firebase-admin');
 admin.initializeApp(functions.config().firebase);
 const express = require('express');
 const cors = require('cors')({origin: true});
 const app = express();
+let result;
 
-// Express middleware that validates Firebase ID Tokens passed in the Authorization HTTP header.
-// The Firebase ID token needs to be passed as a Bearer token in the Authorization HTTP header like this:
-// `Authorization: Bearer <Firebase ID Token>`.
-// when decoded successfully, the ID Token content will be added as `req.user`.
-const validateFirebaseIdToken = (req, res, next) => {
-  console.log('Check if request is authorized with Firebase ID token');
+// var mrequest;
 
-  if (!req.headers.authorization || !req.headers.authorization.startsWith('Bearer ')) {
-    console.error('No Firebase ID token was passed as a Bearer token in the Authorization header.',
-        'Make sure you authorize your request by providing the following HTTP header:',
-        'Authorization: Bearer <Firebase ID Token>');
-    res.status(403).send('Unauthorized');
-    return;
-  }
-  const idToken = req.headers.authorization.split('Bearer ')[1];
-  admin.auth().verifyIdToken(idToken).then(decodedIdToken => {
-    console.log('ID Token correctly decoded', decodedIdToken);
-    req.user = decodedIdToken;
-    next();
-  }).catch(error => {
-    console.error('Error while verifying Firebase ID token:', error);
-    res.status(403).send('Unauthorized');
-  });
-};
 
-app.use(cors);
-app.use(validateFirebaseIdToken);
-app.get('*', (req, res) => {
-  res.send(`Hello ${req.user.name}`);
-});
-
-// This HTTPS endpoint can only be accessed by your Firebase Users.
-// Requests need to be authorized by providing an `Authorization` HTTP header
-// with value `Bearer <Firebase ID Token>`.
-exports.authorizedHello = functions.https.onRequest(app);
+// // Start writing Firebase Functions
+// // https://firebase.google.com/functions/write-firebase-functions
+//
 
 
 
-*/
+
+
+
+
+
+
+// exports.helloWorld = functions.https.onRequest((request, response) => {
+
+//   console.log('Check if request is authorized with Firebase ID token');
+
+//   if (!request.headers.authorization || !request.headers.authorization.startsWith('Bearer ')) {
+//     console.error('No Firebase ID token was passed as a Bearer token in the Authorization header.',
+//         'Make sure you authorize your request by providing the following HTTP header:',
+//         'Authorization: Bearer <Firebase ID Token>');
+//     response.status(403).send('Unauthorized123');
+   
+//   }
+
+//   const idToken = request.headers.authorization.split('Bearer ')[1];
+//   admin.auth().verifyIdToken(idToken).then(decodedIdToken => {
+//     console.log('ID Token correctly decoded', decodedIdToken);
+//     request.user = decodedIdToken;
+//     response.send(request.user);
+//   }).catch(error => {
+//     console.error('Error while verifying Firebase ID token:', error);
+//     response.status(403).send('Unauthorized456');
+//   });
+ 
+//   })
+
+exports.test=functions.https.onRequest((request, response) => {
+const idToken = request.headers.authorization.split('Bearer ')[1];
+console.log("UID from CF="+idToken)
+response.send("UID from CF="+idToken);
+// admin.auth()
+//   .then(function() {
+//   response.send(request.user);
+//   })
+//   .catch(function(error) {
+
+//     console.log("Error creating custom token:", error);
+//     response.status(403).send('Unauthorized456');
+//   });
+
+
+  })
+
+
+
+
+
+
+
+
