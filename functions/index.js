9d9929270bf9b516d739638e8882715a81957d1b
nikhil <ntnikhilesh@gmail.com>
2017-03-20 00:52:00 +0530

    Token  verification done using  middleware function

--- functions/index.js
+++ functions/index.js
@@ -1,43 +1,43 @@
+
+
 'use strict';
 
-let functions = require('firebase-functions');
+const functions = require('firebase-functions');
 const admin = require('firebase-admin');
 admin.initializeApp(functions.config().firebase);
 const express = require('express');
 const cors = require('cors')({origin: true});
 const app = express();
-let mresult;
-let mrequest;
-let mresponse;
 
 
-exports.test=functions.https.onRequest((request, response) => {
+exports.test = functions.https.onRequest((request, response) => 
+{
+
+  cors(request, response, () => 
+  {
+    console.log('inside cors')
 
 
+    //verify token
 
-console.log("UID from CF1="+request.headers.authorization)
+      admin.auth().verifyIdToken(request.headers.authorization).then(decodedIdToken => 
+      {
+          console.log('ID Token correctly decoded', decodedIdToken);
+          request.user = decodedIdToken;
+          console.log("Valid User")
+          response.send("Valid user")
+      }).catch(error => 
+      {
+        console.error('Error while verifying Firebase ID token:', error);
+    
+        response.send("Invalid user")
+    
+      });
 
-
-
- response.send("Please check your log");
-
-
-this.mresult =admin.auth().verifyIdToken(request.headers.authorization).then(decodedIdToken => {
-    console.log('ID Token correctly decoded', decodedIdToken);
-    request.user = decodedIdToken;
-    console.log("Valid User")
-   return 'Valid user';
-  }).catch(error => {
-    console.error('Error while verifying Firebase ID token:', error);
-    response.status(403).send('Unauthorized User');
-    return 'Invalid user'
   });
 
-
-
-  })
-
-
+  
+})
 
 
 
