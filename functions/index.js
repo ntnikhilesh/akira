5d8162466ba6fd0176e05fbab2b9287b9591f661
nikhil <ntnikhilesh@gmail.com>
2017-03-21 00:44:08 +0530

    raw item deleted

--- functions/index.js
+++ functions/index.js
@@ -13,87 +13,8 @@ const app = express();
 exports.test = functions.https.onRequest((request, response) => 
 {
 
-//   cors(request, response, () => 
-//   {
 
-
-// response.send("2 fun executed...")
-// console.log("hi form c1")
-
-// // var MongoClient = require('mongodb').MongoClient
-// //   , assert = require('assert');
-
-// // // Connection URL
-// // var url = 'mongodb://cc:cc123@ds135800.mlab.com:35800/mydb';
-// // // Use connect method to connect to the server
-// // MongoClient.connect(url, function(err, db) {
-// //   assert.equal(null, err);
-// //   console.log("Connected successfully to server");
-
-// //   insertDocuments(db, function() {
-// //     db.close();
-// //   });
-// // });
-
-
-
-
-
-
-
-
-// //     var MongoClient = require('mongodb').MongoClient
-// //   , assert = require('assert');
-
-// // // Connection URL
-// // var url = 'mongodb://cc:cc123@ds135800.mlab.com:35800/mydb';
-
-// // // Use connect method to connect to the server
-// // MongoClient.connect(url, function(err, db) {
-// //   assert.equal(null, err);
-// //   console.log("Connected successfully to server");
-
-// //   db.close();
-// //   response.send("Connected successfully to server")
-// // });
-
-
-
-
-
-    
-
-
-
-
-//     // console.log('inside cors')
-
-
-//     // //verify token
-
-//     //   admin.auth().verifyIdToken(request.headers.authorization).then(decodedIdToken => 
-//     //   {
-//     //       console.log('ID Token correctly decoded', decodedIdToken);
-//     //       request.user = decodedIdToken;
-//     //       console.log("Valid User")
-
-          
-
-//     //       response.send("Valid user")
-//     //   }).catch(error => 
-//     //   {
-//     //     console.error('Error while verifying Firebase ID token:', error);
-    
-//     //     response.send("Invalid user")
-    
-//     //   });
-
-//   });
-
-
-
-
-cors(request, response, () => 
+  cors(request, response, () => 
   {
 
 
@@ -113,7 +34,7 @@ cors(request, response, () =>
           })
 
 
-          //response.send("Valid user")
+          
       }).catch(error => 
       {
         console.error('Error while verifying Firebase ID token:', error);
@@ -125,39 +46,7 @@ cors(request, response, () =>
  
 
 
-    // setConnection(function(result)
-    // {
-    //   response.send(result);
-    // })
-
-
-
-
-  //   console.log("hi form c2")
-
-  //   //response.send("2 fun executed...")
-
-
-  //   var MongoClient = require('mongodb').MongoClient
-  // , assert = require('assert');
-
-  //   // Connection URL
-  //   var url = 'mongodb://cc:cc123@ds135800.mlab.com:35800/mydb';
-  //   // Use connect method to connect to the server
-  //   MongoClient.connect(url, function(err, db) 
-  //   {
-  //       assert.equal(null, err);
-  //       console.log("Connected successfully to server");
-
-  //       insertDocuments(db,function(err,docs)
-  //       {
-  //           if(err)
-  //             response.send(err)
-  //           else
-  //             response.send(docs)
-  //       })
-
-  //   });
+    
 
 
 
@@ -176,9 +65,6 @@ function setConnection(callback)
 
     console.log("hi form c2")
 
-    //response.send("2 fun executed...")
-
-
     var MongoClient = require('mongodb').MongoClient
   , assert = require('assert');
 
@@ -194,10 +80,10 @@ function setConnection(callback)
         {
             if(err)
               callback(err)
-              //response.send(err)
+             
             else
               callback(docs)
-              //response.send(docs)
+              
         })
 
     });
@@ -207,18 +93,21 @@ function setConnection(callback)
 
 
 
-function insertDocuments(db, callback) {
+function insertDocuments(db, callback) 
+{
   // Get the documents collection
   var collection = db.collection('documents');
   // Insert some documents
-  collection.insertMany([
-    {d : 1}, {e : 2}, {f : 3}
-  ], function(err, result) {
-    if(err)
+  collection.insertMany(
+    [
+      {d : 1}, {e : 2}, {f : 3}
+    ], function(err, result) 
+    {
+      if(err)
       callback(err);
-    console.log("Inserted 3 documents into the collection");
-    callback(null,result);
-  });
+      console.log("Inserted 3 documents into the collection");
+      callback(null,result);
+    });
 }
 
 
