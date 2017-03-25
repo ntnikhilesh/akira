cc7b4a4d657c7db1dbb08a6a2a517022774d2aad
nikhil <ntnikhilesh@gmail.com>
2017-03-26 03:33:04 +0530

    CSV file imported on MongoDB using mongoose-write-stream

--- functions/index.js
+++ functions/index.js
@@ -22,6 +22,12 @@ const LOCAL_TMP_FOLDER = '/tmp/';
 // File extension for the created JPEG files.
 const JPEG_EXTENSION = 'jpg';
 
+//Write CSV file
+//var mongoose = require('mongoose');
+var csv      = require('csv-stream');
+//var request  = require('request');
+var zlib     = require('zlib');
+
 
 
 
@@ -135,6 +141,39 @@ exports.testFileUpload = functions.https.onRequest((req, res) =>
 
 
 
+ cors(req, res, () => 
+  {
+
+
+
+ writeCSVData(function(result)
+          {
+            res.send(result);
+          })
+// mongoose.Promise = global.Promise;
+//   mongoose.connect('mongodb://cc:cc123@ds135800.mlab.com:35800/mydb');
+// mongoose.plugin(require('mongoose-write-stream'));
+ 
+// var Tick = mongoose.model('Tick1', {
+//   time: Number,
+//   price: Number,
+//   quantity: Number
+// });
+ 
+// request('http://api.bitcoincharts.com/v1/csv/bitstampUSD.csv.gz')
+//   .pipe(zlib.createGunzip())
+//   .pipe(csv.createStream({
+//     columns: ['time', 'price', 'quantity']
+//   }))
+//   .pipe(Tick.writeStream());
+
+//   res.send("successfully insert...");
+
+
+  })
+
+//mongoimport --db users --collection contacts --type csv --headerline --file /opt/backups/contacts.csv
+
 // var destination = fs.createWriteStream('./savedImage.png');
 // request('https://my.modulus.io/img/modulus-logoSmall-gray20.png').pipe(destination);
 
@@ -193,6 +232,46 @@ exports.testFileUpload = functions.https.onRequest((req, res) =>
 });
 
 
+function writeCSVData()
+{
+  mongoose.Promise = global.Promise;
+  mongoose.connect('mongodb://cc:cc123@ds135800.mlab.com:35800/mydb');
+mongoose.plugin(require('mongoose-write-stream'));
+ console.log("inside fun")
+var Tick = mongoose.model('Tick', {
+  
+  time: Number,
+  price: Number,
+  quantity: Number
+});
+ 
+request('http://api.bitcoincharts.com/v1/csv/bitstampUSD.csv.gz')
+  .pipe(zlib.createGunzip())
+  .pipe(csv.createStream({
+    
+    columns: ['time', 'price', 'quantity']
+  }))
+  .pipe(Tick.writeStream()),function()
+  {
+    console.log("at last ....");
+    callback("successfully insert..");
+  }
+  
+  
+  
+  
+}
+
+
+
+
+
+
+
+
+
+
+
 
 function setmConnection(callback)
 {
@@ -248,56 +327,56 @@ fluffy.save(function (err, fluffy) {
 
 
 
-exports.imageToJPG = functions.storage.object().onChange(event => {
-  const object = event.data;
-  const filePath = object.name;
-  const filePathSplit = filePath.split('/');
-  const fileName = filePathSplit.pop();
-  const fileNameSplit = fileName.split('.');
-  const fileExtension = fileNameSplit.pop();
-  const baseFileName = fileNameSplit.join('.');
-  const fileDir = filePathSplit.join('/') + (filePathSplit.length > 0 ? '/' : '');
-  const JPEGFilePath = `${fileDir}${baseFileName}.${JPEG_EXTENSION}`;//
-  const tempLocalDir = `${LOCAL_TMP_FOLDER}${fileDir}`;
-  const tempLocalFile = `${tempLocalDir}${fileName}`;//
-  const tempLocalJPEGFile = `${LOCAL_TMP_FOLDER}${JPEGFilePath}`;//
+// exports.imageToJPG = functions.storage.object().onChange(event => {
+//   const object = event.data;
+//   const filePath = object.name;
+//   const filePathSplit = filePath.split('/');
+//   const fileName = filePathSplit.pop();
+//   const fileNameSplit = fileName.split('.');
+//   const fileExtension = fileNameSplit.pop();
+//   const baseFileName = fileNameSplit.join('.');
+//   const fileDir = filePathSplit.join('/') + (filePathSplit.length > 0 ? '/' : '');
+//   const JPEGFilePath = `${fileDir}${baseFileName}.${JPEG_EXTENSION}`;//
+//   const tempLocalDir = `${LOCAL_TMP_FOLDER}${fileDir}`;
+//   const tempLocalFile = `${tempLocalDir}${fileName}`;//
+//   const tempLocalJPEGFile = `${LOCAL_TMP_FOLDER}${JPEGFilePath}`;//
 
-  // Exit if this is triggered on a file that is not an image.
-  if (!object.contentType.startsWith('image/')) {
-    console.log('This is not an image.');
-    return;
-  }
+//   // Exit if this is triggered on a file that is not an image.
+//   if (!object.contentType.startsWith('image/')) {
+//     console.log('This is not an image.');
+//     return;
+//   }
 
-  // Exit if the image is already a JPEG.
-  if (object.contentType.startsWith('image/jpeg')) {
-    console.log('Already a JPEG.');
-    return;
-  }
+//   // Exit if the image is already a JPEG.
+//   if (object.contentType.startsWith('image/jpeg')) {
+//     console.log('Already a JPEG.');
+//     return;
+//   }
 
-  // Exit if this is a move or deletion event.
-  if (object.resourceState === 'not_exists') {
-    console.log('This is a deletion event.');
-    return;
-  }
+//   // Exit if this is a move or deletion event.
+//   if (object.resourceState === 'not_exists') {
+//     console.log('This is a deletion event.');
+//     return;
+//   }
 
-  // Create the temp directory where the storage file will be downloaded.
-  return mkdirp(tempLocalDir).then(() => {
-    // Download file from bucket.
-    const bucket = gcs.bucket(object.bucket);
-    return bucket.file(filePath).download({
-      destination: tempLocalFile
-    }).then(() => {
-      console.log('The file has been downloaded to', tempLocalFile);
-      // Convert the image to JPEG using ImageMagick.
-      return exec(`convert "${tempLocalFile}" "${tempLocalJPEGFile}"`).then(() => {
-        console.log('JPEG image created at', tempLocalJPEGFile);
-        // Uploading the JPEG image.
-        return bucket.upload(tempLocalJPEGFile, {
-          destination: JPEGFilePath
-        }).then(() => {
-          console.log('JPEG image uploaded to Storage at', filePath);
-        });
-      });
-    });
-  });
-});
+//   // Create the temp directory where the storage file will be downloaded.
+//   return mkdirp(tempLocalDir).then(() => {
+//     // Download file from bucket.
+//     const bucket = gcs.bucket(object.bucket);
+//     return bucket.file(filePath).download({
+//       destination: tempLocalFile
+//     }).then(() => {
+//       console.log('The file has been downloaded to', tempLocalFile);
+//       // Convert the image to JPEG using ImageMagick.
+//       return exec(`convert "${tempLocalFile}" "${tempLocalJPEGFile}"`).then(() => {
+//         console.log('JPEG image created at', tempLocalJPEGFile);
+//         // Uploading the JPEG image.
+//         return bucket.upload(tempLocalJPEGFile, {
+//           destination: JPEGFilePath
+//         }).then(() => {
+//           console.log('JPEG image uploaded to Storage at', filePath);
+//         });
+//       });
+//     });
+//   });
+// });
